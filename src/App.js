b818b8bdb28d0a8baaef61eccd15222cdc58402b
kangxx <kangxx@sany.com.cn>
2024-05-31 15:15:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Button from "./Button";
+import Chess from "./Chess";
 
+const isShowButtom = false;
+const list = [1, 2, 3, 5];
 function App() {
+  const [count, setCount] = useState(0);
+  const [allCount, setAllCount] = useState(0);
+  const handleClick = (item) => {
+    console.log(item, "iitem");
+    setCount(count + 1);
+  };
+
+  const clickBtn = () => {
+    setAllCount(allCount + 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chess></Chess>
+        {isShowButtom ? <Button></Button> : <span>null</span>}
+        <Button allCount={allCount} clickBtn={clickBtn}></Button>
+        <Button allCount={allCount} clickBtn={clickBtn}></Button>
+        {list.map((item) => (
+          <span
+            key={item}
+            onClick={() => {
+              handleClick(item);
+            }}
+          >
+            {item}+'listItem'+{count + item}
+          </span>
+        ))}
       </header>
     </div>
   );
